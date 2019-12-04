2cff10a52b5d620b616b741ca7f0411b54b1d877
Ivanich85 <ivan.dmitriev85@gmail.com>
2019-12-04 23:57:56 +0300

    Test DB added and configured. All tests performing there

--- src/test/resources/db/populateDB_test.sql
+++ src/test/resources/db/populateDB_test.sql
@@ -3,9 +3,11 @@ DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
-  ('User-1', 'user-1@yandex.ru', 'password'),
+  ('User', 'user@yandex.ru', 'password'),
   ('Admin', 'admin@gmail.com', 'admin');
 
 INSERT INTO user_roles (role, user_id) VALUES
 ('ROLE_USER', 100000),
 ('ROLE_ADMIN', 100001);
+
+select * from USER_ROLES;
